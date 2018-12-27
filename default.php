52a9df9e8d4c26522990d8f68baac8ec986c1923
you133 <45588463+you133@users.noreply.github.com>
2018-12-27 16:40:18 +0700

    Update and rename index.php to default.php

---
+++ default.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>X-K1R4
+    </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+    </script>
+  </head>
+  <body>
+    <center>
+      <div class="container">
+        <h2>MR-K4MVR3T
+        </h2>
+        <hr>
+        <style>
+          body {
+            background-color:white;
+            color:black
+          }
+          .content {
+            max-width: 700px;
+            margin: auto;
+          }
+        </style>
+        </head>
+      <form action method="get">
+        <input type="hidden" name="action" value="login" />
+        <div class="form-group">
+          <div class="input-group">
+            <span class="input-group-addon">Password :
+            </span>
+            <input type="password" placeholder="" class="form-control" name="pass" autofocus="" required="">
+          </div>
+        </div>
+        <button type="submit" style="width: 100%;" class="btn btn-danger" name="submit">Login
+        </button>
+      </form>
+      <hr>
+      <footer>
+        <p class="text-center">Copyright &copy; 2017 | 
+          <a href="http://www.k4mvr3t.tk">MR-K4MVR3T
+          </a>
+        </p>
+      </footer>
+      </div>
+    </center>
+    </body>
+</html>
